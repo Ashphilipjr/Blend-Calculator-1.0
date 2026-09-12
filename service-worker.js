e5ed8556f979cdaaa4ef13e7a58b92b605542ff2
Ashphilipjr <ashwin20001@gmail.com>
2026-09-12 15:20:23 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'alloy-costing-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network-first for CDN scripts (React/Babel), cache-first for app shell
+  const isAppShell = ASSETS.some((a) => event.request.url.endsWith(a.replace('./', '')));
+  if (isAppShell) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request)
+        .then((res) => {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+          return res;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+});
